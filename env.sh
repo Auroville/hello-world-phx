69fb389c7fb22481853436fbebd00faf284916fd
AM <maj8al@gmail.com>
2021-09-30 09:28:59 +0000

    added more env

--- env.sh
+++ env.sh
@@ -2,10 +2,15 @@
 
 #set gitpod environment variables (scope is project). These are default values.
 
-gp env DATABASE_URL="postgresql://gitpod@localhost"
 gp env PGHOSTADDR="127.0.0.1"
-gp env PGDATABASE="postgres"
+gp env PGDATABASE="hello_world_dev"
 gp env PGDATA="/workspace/.pgsql/data"
+gp env PGHOST="localhost"
+gp env PGPORT="5432"
+gp env PGUSER="gitpod"
 
 #secrets
-gp env SECRET_KEY_BASE=N5tC9rMlM2ETJ2U8HFYXF8vdeBrLp6sDjNMtmnxxz350nAugLFbaFkemAUdWsscW
+gp env SECRET_KEY_BASE="N5tC9rMlM2ETJ2U8HFYXF8vdeBrLp6sDjNMtmnxxz350nAugLFbaFkemAUdWsscW"
+gp env PGPASSWORD="postgres"
+
+gp env DATABASE_URL="postgresql://{$PGUSER}:{$PGPASSWORD}@{$PGHOST}:{$PGPORT}/{$PGDATABASE}"
